8ecac2da205b2b977a41561e546c6c9ada2ff7d2
DESKTOP-KP0GSC5\heyunchao <yunchaohe@gmail.com>
2017-03-18 14:51:07 +0800

    tf debug

---
+++ 24_linear_regression.py
@@ -0,0 +1,90 @@
+'''
+A linear regression learning algorithm example using TensorFlow library.
+
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+rng = np.random
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 1000
+display_step = 50
+
+data_X = np.arange(150)
+true_w, true_b = 13, 3
+data_Y = data_X * true_w + true_b + rng.rand(len(data_X)) * 100
+
+# Training Data
+train_X = data_X[:-100]
+train_Y = data_Y[:-100]
+n_samples = train_X.shape[0]
+
+# tf Graph Input
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+# Set model weights
+W = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
+
+# Construct a linear model
+pred = tf.add(tf.multiply(X, W), b)
+
+# Mean squared error
+cost = tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * n_samples)
+# Gradient descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for (x, y) in zip(train_X, train_Y):
+            sess.run(optimizer, feed_dict={X: x, Y: y})
+
+        # Display logs per epoch step
+        if (epoch + 1) % display_step == 0:
+            c = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+            print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(c), \
+                  "W=", sess.run(W), "b=", sess.run(b))
+
+    print("Optimization Finished!")
+    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+
+    # Graphic display
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
+
+    # Testing example, as requested (Issue #2)
+    test_X = data_X[-100:]
+    test_Y = data_Y[-100:]
+
+    print("Testing... (Mean square loss Comparison)")
+    testing_cost = sess.run(
+        tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
+        feed_dict={X: test_X, Y: test_Y})  # same function as cost above
+
+    print("Testing cost=", testing_cost)
+    print("Absolute mean square loss difference:", abs(
+        training_cost - testing_cost))
+
+    plt.plot(test_X, test_Y, 'bo', label='Testing data')
+    plt.plot(test_X, sess.run(W) * test_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
